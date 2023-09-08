fcf4956f642fdc7b9063d1bce9158e6e98b41b42
Andrii Chernenko <andrii.chernenko@schibsted.com>
2023-09-08 12:27:10 +0200

    Initial Commit

---
+++ MacroTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacroTest
+//
+//  Created by Andrii Chernenko on 2023-09-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacroTest/MacroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroTestApp.swift
+//  MacroTest
+//
+//  Created by Andrii Chernenko on 2023-09-08.
+//
+
+import SwiftUI
+
+@main
+struct MacroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
